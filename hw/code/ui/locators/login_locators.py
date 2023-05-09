3c951dd56794d94b47af4ab106d7ebc6e52fdce0
Aint <pinevichv@gmail.com>
2023-05-09 15:38:16 +0300

    cleared

--- hw/code/ui/locators/login_locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginPageLocators():
-    EMAIL_INPUT_LOGIN_PAGE = (By.NAME, 'email')
-
-    PASSWORD_INPUT_LOGIN_PAGE = (By.NAME, 'password')
-
-    SIGN_IN_BUTTON_LOGIN_PAGE = (By.ID, 'submit-result')
